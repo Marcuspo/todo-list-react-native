849e97f35849fe231d52ab9c606c77ff226a2a61
Marcus Pontes <marcusporama@gmail.com>
2020-12-28 12:21:56 -0300

    initial

---
+++ app/App.js
@@ -0,0 +1,36 @@
+import React, {Component} from 'react';
+import {View, ScrollView, StyleSheet} from 'react-native';
+
+class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      inputValue: '',
+      todos: [],
+      type: 'All',
+    };
+  }
+
+  render() {
+    return (
+      <View style={styles.container}>
+        <ScrollView keyboardShouldPersistTaps="always" style={styles.content}>
+          <View />
+        </ScrollView>
+      </View>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#f5f5f5',
+  },
+  content: {
+    flex: 1,
+    paddingTop: 60,
+  },
+});
+
+export default App;
